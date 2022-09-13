f39d9b34b119d45b3e4c3c0de3305883a88ecb2b
aletexis-PROME <111149638+aletexis-PROME@users.noreply.github.com>
2022-09-13 11:43:11 -0300

    -delete: loginComponent -fix: routing para home
    modificaciones en /code

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 // import { GoogleLoginProvider, SocialLoginModule } from 'angularx-social-login';
 import { CookieService } from 'ngx-cookie-service';
@@ -12,7 +12,7 @@ import { CookieService } from 'ngx-cookie-service';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+    // LoginComponent,
     HomeComponent,
   ],
   imports: [
